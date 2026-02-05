eaf3f827d9841e164850c8635d2cf9d79c70af59
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2026-02-04 19:20:36 -0500

    feat: cree el componente inicio

--- Frontend/angular/src/app/inicio/inicio.ts
+++ Frontend/angular/src/app/inicio/inicio.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-inicio',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './inicio.html',
   styleUrl: './inicio.scss',
 })
