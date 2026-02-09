0b773ae443a113a7f90fd975c5afc763212a2941
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2026-02-09 16:05:57 -0500

    feat: cree el componente de...

---
+++ Frontend/angular/src/app/demo/pages/lock-screen/lock-screen.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lock-screen',
+  imports: [],
+  templateUrl: './lock-screen.html',
+  styleUrl: './lock-screen.scss',
+})
+export class LockScreen {
+
+}
