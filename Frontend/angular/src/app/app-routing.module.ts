4f82f970ffd34400e63b8b3c51b8d9e6d116b4ca
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2026-02-09 16:06:55 -0500

    feat: añadi a las rutas el perfil

--- Frontend/angular/src/app/app-routing.module.ts
+++ Frontend/angular/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './demo/layout/admin';
 import { EmptyComponent } from './demo/layout/empty';
 import { Inicio } from './inicio/inicio';
+import { ProfileComponent } from './demo/pages/profile/profile';
 
 const routes: Routes = [
   {
@@ -13,6 +14,10 @@ const routes: Routes = [
         path: '',
         redirectTo: '/dashboard',
         pathMatch: 'full'
+      },
+      {
+        path: 'profile',
+        component: ProfileComponent
       },
       {
         path: 'Inicio',
