5e4015c799bf26dcb86a8cc9589205822b0c832d
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2026-02-10 15:17:34 -0500

    fix: corregi y añadi algunas columnas de la tabla proveedores

--- Backend/Database/Migration/Proveedores.sql
+++ Backend/Database/Migration/Proveedores.sql
@@ -2,7 +2,12 @@
 CREATE TABLE proveedores (
     id SERIAL PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
-    descripcion TEXT,
+    observaciones TEXT,
+    nit VARCHAR(20) UNIQUE NOT NULL,
+    correo VARCHAR(100) UNIQUE NOT NULL,
+    telefono VARCHAR(20),
+    ciudad VARCHAR(50),
+    estado BOOLEAN DEFAULT TRUE,
     fecha_llegada DATE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
