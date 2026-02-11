cf005cc4bab2917a6e68348b7a145f0ea87ec1ce
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2026-02-10 19:54:08 -0500

    feat: añadi otra ruta que es para crear productos

--- Backend/src/Routes/Productos.routes.php
+++ Backend/src/Routes/Productos.routes.php
@@ -1,3 +1,6 @@
 <?php 
-
+use App\Controllers\ProductosController;
+// GET - Obtener todos los productos
+$router->get('/api/productos', [ProductosController::class, 'getAllProductos']);
+$router->post('/api/CrearProductos', [ProductosController::class, 'crearProducto']);
 ?>
