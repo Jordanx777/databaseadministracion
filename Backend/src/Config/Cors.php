717e17fc9afee94012c09bea4acc51a9b1eb19b4
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2026-02-10 19:53:26 -0500

    fix: añadi una condiconal para mas a delante produccion

--- Backend/src/Config/Cors.php
+++ Backend/src/Config/Cors.php
@@ -3,7 +3,13 @@ namespace App\Config;
 
 class Cors {
     public static function handle(): void {
-        $allowedOrigins = explode(',', $_ENV['FRONTEND_URL'] ?? 'http://localhost:4200');
+        if($_ENV['APP_ENV'] === 'development') {
+            $allowedOrigins = explode(',', $_ENV['FRONTEND_URL'] ?? 'http://localhost:4200');
+        } else {
+                $allowedOrigins = explode(',', $_ENV['FRONTEND_URL'] ?? 'https://inventario-frontend.vercel.app');
+            }
+
+        // $allowedOrigins = explode(',', $_ENV['FRONTEND_URL'] ?? 'http://localhost:4200');
         
         $origin = $_SERVER['HTTP_ORIGIN'] ?? '';
         
