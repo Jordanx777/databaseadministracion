c0df8b50f9d303ac78f74ce0978bcf4ec78159fc
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2026-02-12 23:04:09 -0500

    feat: añadi las rutas restantes para el crud

--- Backend/src/Routes/Productos.routes.php
+++ Backend/src/Routes/Productos.routes.php
@@ -3,4 +3,7 @@ use App\Controllers\ProductosController;
 // GET - Obtener todos los productos
 $router->get('/api/productos', [ProductosController::class, 'getAllProductos']);
 $router->post('/api/CrearProductos', [ProductosController::class, 'crearProducto']);
+$router->get('/api/productos/{id}', [ProductosController::class, 'getProductoById']);
+$router->put('/api/ActualizarProductos/{id}', [ProductosController::class, 'actualizarProducto']);
+$router->delete('/api/deleteproductos/{id}', [ProductosController::class, 'deleteProducto']);
 ?>
