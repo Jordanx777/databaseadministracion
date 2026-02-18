84284ed6bdac99cf94b9f2ac362abbedfafd2b31
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2026-02-18 18:07:07 -0500

    feat: añadi la ruta de el router que tiene las rutas  de los clientes

--- Backend/public/index.php
+++ Backend/public/index.php
@@ -33,6 +33,7 @@ try {
     require_once __DIR__ . '/../src/Routes/Subcategorias.routes.php';
     require_once __DIR__ . '/../src/Routes/Proveedores.routes.php';
     require_once __DIR__ . '/../src/Routes/Productos.routes.php';
+    require_once __DIR__ . '/../src/Routes/Clientes.router.php';
     
     // Ejecutar router
     $router->run();
