386ddfdf3e9cd374ad0aa921227dd2a14cecc341
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2026-02-18 23:12:17 -0500

    feat: agregue la ruta para buscar al cliente

--- Backend/src/Routes/Clientes.router.php
+++ Backend/src/Routes/Clientes.router.php
@@ -4,6 +4,9 @@ use App\Controllers\ClientesController;
 $router->get('/api/clientes', [ClientesController::class, 'getAllClientes']);
 // $router->get('/api/clientes/{id}', [ClientesController::class, 'getClienteById']);
 $router->post('/api/clientes/agregar', [ClientesController::class, 'createCliente']);
+$router->get('/api/clientes/buscar', [ClientesController::class, 'searchClientes']);
 $router->put('/api/clientes/actualizar/{id}', [ClientesController::class, 'updateCliente']);
 $router->delete('/api/clientes/eliminar/{id}', [ClientesController::class, 'deleteCliente']);
+// buscador por nombre de los clientes el enpoin http://localhost:8080/api/clientes/buscar?query= nombre
+
 ?>
