a856ee8ded8d71f9e8c774be87f945edadb628bf
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2026-02-21 11:55:04 -0500

    añadi las rutas para obtener las ventas

--- Backend/src/Routes/Ventas.routes.php
+++ Backend/src/Routes/Ventas.routes.php
@@ -2,8 +2,8 @@
 use App\Controllers\VentasController;
 
 $router->post('/api/ventas', [VentasController::class, 'crear']);
-$router->get('/api/ventas', [VentasController::class, 'obtenerTodas']);
-$router->get('/api/ventas/{id}', [VentasController::class, 'obtenerPorId']);
-$router->put('/api/ventas/{id}/cancelar', [VentasController::class, 'cancelar']);
+$router->get('/api/ventas/completas', [VentasController::class, 'obtenerVentasCompletas']);
+$router->get('/api/ventas/completas/{id}', [VentasController::class, 'obtenerVentaCompletaPorId']);
+$router->put('/api/ventas/cancelar/{id}', [VentasController::class, 'cancelar']);
 
 ?>
