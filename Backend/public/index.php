5e386442838835afc6cd81b70a75a16653e1a1f7
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2026-02-21 22:32:06 -0500

    fix: añadi algo para evitar que el backend mande html pero lo comente

--- Backend/public/index.php
+++ Backend/public/index.php
@@ -1,6 +1,10 @@
 <?php 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+// ini_set('display_errors', 0);
+// error_reporting(E_ALL);
+// header('Content-Type: application/json');
+
 use App\Core\Router;
 use App\Config\Cors;
 
