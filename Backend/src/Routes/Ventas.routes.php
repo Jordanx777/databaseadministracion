747a07496b5d8868fcd2f4bcb954776eb6e79b49
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2026-02-25 17:21:19 -0500

    fix: estuve depuando

--- Backend/src/Routes/Ventas.routes.php
+++ Backend/src/Routes/Ventas.routes.php
@@ -5,5 +5,6 @@ $router->post('/api/ventas', [VentasController::class, 'crear']);
 $router->get('/api/ventas/completas', [VentasController::class, 'obtenerVentasCompletas']);
 $router->get('/api/ventas/completas/{id}', [VentasController::class, 'obtenerVentaCompletaPorId']);
 $router->put('/api/ventas/cancelar/{id}', [VentasController::class, 'cancelar']);
+$router->get('api/pagos/venta/{id}', [VentasController::class,'listarPorVenta']);
 
 ?>
