c0fa35f94eacf2d131baa18a128184dff99b44a7
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2026-02-25 16:36:27 -0500

    añadi la ruta de los pagos

--- Backend/public/index.php
+++ Backend/public/index.php
@@ -40,6 +40,7 @@ try {
     require_once __DIR__ . '/../src/Routes/Clientes.router.php';
     require_once __DIR__ . '/../src/Routes/Ventas.routes.php';
     require_once __DIR__ . '/../src/Routes/Cuentas.routes.php';
+    require_once __DIR__ . '/../src/Routes/Pagos.routes.php';
     
     // Ejecutar router
     $router->run();
