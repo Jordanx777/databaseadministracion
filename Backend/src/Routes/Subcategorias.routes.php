b0e69c706ef52a4dc8b4ded68c9a650eb56dc9aa
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2026-02-28 23:48:40 -0500

    feat: añadi el resto de rutas pa el crud

--- Backend/src/Routes/Subcategorias.routes.php
+++ Backend/src/Routes/Subcategorias.routes.php
@@ -1,4 +1,12 @@
 <?php
 use App\Controllers\SubcategoriasController;
+// Rutas para subcategorías
 $router->get('/api/subcategorias', [SubcategoriasController::class, 'getAllSubcategorias']);
+//  Obtener subcategorías de una categoría específica
+$router->get('/api/subcategorias/categoria/{categoriaId}', [SubcategoriasController::class, 'getSubcategoriasPorCategoria']);
+// Obtener una subcategoría por su ID
+$router->get('/api/subcategorias/{id}', [SubcategoriasController::class, 'getSubcategoriaById']);
+$router->post('/api/subcategorias', [SubcategoriasController::class, 'createSubcategoria']);
+$router->put('/api/subcategorias/{id}', [SubcategoriasController::class, 'updateSubcategoria']);
+$router->delete('/api/subcategorias/{id}', [SubcategoriasController::class, 'deleteSubcategoria']);
 ?>
