4d8955d8bcbb4607d799c0b96176aff6a8792beb
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2026-03-04 00:14:03 -0500

    feat: cree el servicio para los roles

---
+++ Frontend/angular/src/app/@theme/services/Roles.services.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ApiService } from './api.service';
+
+export interface Rol {
+  id_rol?: number;
+  nombre: string;
+  descripcion?: string;
+  activo?: boolean;
+  fecha_creacion?: string;
+  fecha_actualizacion?: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RolesService {
+
+  constructor(private apiService: ApiService) {}
+
+  getRoles(): Observable<any> {
+    return this.apiService.get('roles');
+  }
+
+  getRol(id: number): Observable<any> {
+    return this.apiService.get(`roles/${id}`);
+  }
+
+  createRol(rol: Rol): Observable<any> {
+    console.log("Roles: ",rol);
+    return this.apiService.post('roles/crear', rol);
+  }
+
+  updateRol(id: number, rol: Rol): Observable<any> {
+    console.log("Actualizar: ",rol);
+    return this.apiService.put(`roles/actualizar/${id}`, rol);
+  }
+
+  deleteRol(id: number): Observable<any> {
+    return this.apiService.delete(`roles/eliminar/${id}`);
+  }
+
+  getRolesActivos(): Observable<any> {
+    return this.apiService.get('roles/activos');
+  }
+}
