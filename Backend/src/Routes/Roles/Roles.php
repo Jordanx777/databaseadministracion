a6b39b7652ad3805f4638dff3b98ae0faf6bf390
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2026-03-04 00:13:27 -0500

    feat: cree las rutas restantes

--- Backend/src/Routes/Roles/Roles.php
+++ Backend/src/Routes/Roles/Roles.php
@@ -1,4 +1,11 @@
 <?php
 use App\Controllers\RolesController; 
 
-$router->get('/api/roles', [RolesController::class, 'index']);
+$router->get('/api/roles', [RolesController::class, 'getAllRoles']);
+// $router->get('/api/roles/activos', [RolesController::class, 'getAllRoles']);
+$router->get('/api/roles/{id}', [RolesController::class, 'getRolById']);
+$router->post('/api/roles/crear', [RolesController::class, 'createRol']);
+$router->put('/api/roles/actualizar/{id}', [RolesController::class, 'updateRol']);
+$router->delete('/api/roles/eliminar/{id}',[RolesController::class, 'deleteRol']);
+
+?>
