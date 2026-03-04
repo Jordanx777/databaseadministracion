1e423ecab6360e5576eb5f368fa82a4347f70680
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2026-03-03 21:01:30 -0500

    feat: añadi el resto de rutas para el crud de las categorias

--- Backend/src/Routes/Marcas.routes.php
+++ Backend/src/Routes/Marcas.routes.php
@@ -2,8 +2,8 @@
 use App\Controllers\MarcasController;
 
 $router->get('/api/marcas', [MarcasController::class, 'getAllMarcas']);
-// $router->get('/api/marcas/{id}', [MarcasController::class, 'getMarcaById']);
-// $router->post('/api/marcas', [MarcasController::class, 'createMarca']);
-// $router->put('/api/marcas/{id}', [MarcasController::class, 'updateMarca']);
-// $router->delete('/api/marcas/{id}', [MarcasController::class, 'deleteMarca']);
+$router->get('/api/marcas/{id}', [MarcasController::class, 'getMarcaById']);
+$router->post('/api/marcas/crear', [MarcasController::class, 'createMarca']);
+$router->put('/api/marcas/actualizar/{id}', [MarcasController::class, 'updateMarca']);
+$router->delete('/api/marcas/eliminar/{id}', [MarcasController::class, 'deleteMarca']);
 ?>
