95df48f05317f6edc4e2673d8680d9b5c5414682
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2026-03-16 22:38:04 -0500

    feat: cree la ruta para cambiar la contraseña

--- Backend/src/Routes/Auth/Auth.php
+++ Backend/src/Routes/Auth/Auth.php
@@ -1,9 +1,12 @@
 <?php
 use App\Controllers\AuthController;
+use App\Controllers\UsuariosController;
 
 $router->post('/api/auth/register', [AuthController::class, 'register']);
 $router->post('/api/auth/login', [AuthController::class, 'login']);
 $router->post('/api/auth/logout', [AuthController::class, 'logout']);
 $router->get('/api/auth/me', [AuthController::class, 'me']); // Obtener usuario actual
+
+$router->post('/api/auth/change-password', [UsuariosController::class, 'changePassword']);
 
 ?>
