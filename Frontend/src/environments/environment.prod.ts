74ca97f827f83634246a43dcf84f280bb765f6cc
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2026-03-20 21:04:55 -0500

    fix: corregi la ruta

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: true,
-  apiUrl: 'https://https://inventario-backend-wrir.onrender.com/api/',
-  imagesUrl: 'https://https://inventario-backend-wrir.onrender.com',
+  apiUrl: 'https://inventario-backend-wrir.onrender.com/api/',
+  imagesUrl: 'https://inventario-backend-wrir.onrender.com',
 };
