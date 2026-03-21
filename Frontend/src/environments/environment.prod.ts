60ce9def72a1691bd30a9e317ee0a1c3dc9ae2ec
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2026-03-20 20:23:09 -0500

    fix: le pase las variables de entorno

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -2,5 +2,7 @@ import packageInfo from '../../package.json';
 
 export const environment = {
   appVersion: packageInfo.version,
-  production: true
+  production: true,
+  apiUrl: 'https://https://inventario-backend-wrir.onrender.com/api/',
+  imagesUrl: 'https://https://inventario-backend-wrir.onrender.com',
 };
